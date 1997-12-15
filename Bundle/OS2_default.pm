e1f95438bbbb45f35ea8950c59337c590f071117
Ilya Zakharevich <cpan@ilyaz.org>
1997-12-15 17:49:00 +0000

    Import of ILYAZ/Bundle-OS2_default-1.01 from CPAN.
    gitpan-cpan-distribution: Bundle-OS2_default
    gitpan-cpan-version:      1.01
    gitpan-cpan-path:         ILYAZ/modules/Bundle-OS2_default-1.01.tar.gz
    gitpan-cpan-author:       ILYAZ
    gitpan-cpan-maturity:     released

---
+++ Bundle/OS2_default.pm
@@ -0,0 +1,26 @@
+package Bundle::OS2_default;
+
+$VERSION = '1.01';
+
+1;
+
+=head1 NAME
+
+Bundle::OS2_default - Modules to install last for OS/2 binary distribution
+
+=head1 SYNOPSIS
+
+  perl -MCPAN -e "install Bundle::OS2_default"
+
+  perl_ -MCPAN -e "force 'install', Bundle::OS2_default1"
+  perl_ -MCPAN -e "force 'install', Bundle::OS2_default2"
+  perl_ -MCPAN -e "force 'install', Bundle::OS2_default3"
+
+=head1 CONTENTS
+
+Bundle::OS2_default1_2
+
+Bundle::OS2_default3
+
+=cut
+
---
+++ Bundle/OS2_default1.pm
@@ -0,0 +1,36 @@
+package Bundle::OS2_default1;
+
+$VERSION = '1.00';
+
+1;
+
+=head1 NAME
+
+Bundle::OS2_default1 - Modules to install first for OS/2 binary distribution
+
+=head1 SYNOPSIS
+
+  perl -MCPAN -e "install Bundle::OS2_default1"
+
+=head1 CONTENTS
+
+Data::Flow
+
+Term::ReadKey
+
+Devel::DProf
+
+Devel::Peek
+
+Curses
+
+Data::Dumper		- to get libnet
+
+MD5			- to get libnet
+
+GD
+
+FreezeThaw
+
+=cut
+
